4955e7333fe5af443e955f6ef3fd52eac70dd804
unknown <pda1708@gmail.com>
2023-12-14 17:03:05 -0300

    first commit

---
+++ src/views/home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Image from 'next/image'
+
+
+const Home = () => {
+    return (
+        <div className='logo-dst'>
+        <span>
+            <Image src="/mariposo.png" width="900" height="600"/>
+        </span>
+        </div>
+
+
+    )
+}
+
+
+export default Home;
